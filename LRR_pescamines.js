f242f2074f379380b2aa45c87a207216e4c7b103
LaiaRodri <laia.rodriguez@enti.cat>
2023-11-15 19:55:05 +0100

    Initial commit with functions

---
+++ LRR_pescamines.js
@@ -0,0 +1,31 @@
+let x;
+let y;
+let nMinesAdj;
+
+function iniciarPartida(){
+
+}
+
+function crearTaulell(){
+
+}
+
+function obreCasella(){
+
+}
+
+function setMines(){
+
+}
+
+function calculaAdjacents(){
+
+}
+
+function esMina(x,y){
+
+}
+
+function setMinesAdjacents(x, y, nMinesAdj){
+
+}
